ff682dc2e34719874e359ecca7cbd40dc58e88e0
yanyan7 <sakuraperfection@gmail.com>
2021-01-21 11:37:46 +0900

    プレビュー表示完了

--- app/mailers/sample_mailer.rb
+++ app/mailers/sample_mailer.rb
@@ -1,6 +1,5 @@
 class SampleMailer < ApplicationMailer
-  # Preview this email at
-  # http://localhost:3000/rails/mailers/sample_mailer/send_mail
+  # Preview all emails at http://localhost:3000/rails/mailers/sample_mailer
   # def send_mail(to, subject)
   def send_mail
     mail(
