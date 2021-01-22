6cef93ea7c892ce92bcdb51d0d85dbf82bd06607
yanyan7 <sakuraperfection@gmail.com>
2021-01-22 11:33:39 +0900

    完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => 587,
-    :user_name => 'sakuraperfection@gmail.com',
-    :password => 'jwqogflmpjimayyw',
+    :user_name => 'Gmailのメアド',
+    :password => 'アプリパスワード',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
