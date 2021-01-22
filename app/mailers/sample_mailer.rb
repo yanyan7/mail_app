0eea8154c39d77562b173a015376782d114ce5cf
yanyan7 <sakuraperfection@gmail.com>
2021-01-22 11:30:30 +0900

    完了

--- app/mailers/sample_mailer.rb
+++ app/mailers/sample_mailer.rb
@@ -1,6 +1,7 @@
 class SampleMailer < ApplicationMailer
   # Preview all emails at http://localhost:3000/rails/mailers/sample_mailer
   def send_mail(to, subject, text)
+    @subject = subject
     @text = text
     mail(
       to:   to,
